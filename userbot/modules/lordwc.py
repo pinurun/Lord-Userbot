e215659beefc05ca06a33a41d6622b8f381a11ad
P i n u r u n <81405677+pinurun@users.noreply.github.com>
2021-03-31 20:18:40 +0700

    Update lordwc.py

--- userbot/modules/lordwc.py
+++ userbot/modules/lordwc.py
@@ -1,4 +1,4 @@
-s# Copyright (C) 2020 Alfiananda P.A
+# Copyright (C) 2020 Alfiananda P.A
 #
 # Licensed under the General Public License, Version 3.0;
 # you may not use this file except in compliance with the License.
